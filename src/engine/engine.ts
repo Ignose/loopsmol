7124c180802130dd54e37486d9472e861121a783
Ignose <ihpares@gmail.com>
2024-04-26 17:03:55 -0400

    Myup

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -803,7 +803,7 @@ export class Engine extends BaseEngine<CombatActions, ActiveTask> {
       autoAbortThreshold: "-0.05",
       recoveryScript: "",
       removeMalignantEffects: false,
-      choiceAdventureScript: "loopsmol_choice.ash",
+      choiceAdventureScript: "smoliscasual_choice.ash",
       mpAutoRecoveryItems: ensureRecovery(
         "mpAutoRecoveryItems",
         ["black cherry soda", "doc galaktik's invigorating tonic"],
